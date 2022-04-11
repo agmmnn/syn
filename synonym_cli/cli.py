f81cf486637c37b58ac0f11d77659bdffff64a51
Gökçe <agmerdun@hotmail.com>
2022-04-12 02:14:30 +0300

    dependencies reduced (rich and gazpacho)

--- synonym_cli/cli.py
+++ synonym_cli/cli.py
@@ -1,8 +1,9 @@
-import requests
 from urllib.parse import quote
-from bs4 import BeautifulSoup
-import json
+from json import loads
+from gazpacho import Soup
+import http.client
 from rich.table import Table
+from rich.console import Console
 from rich import box
 from rich import print as rprint
 
@@ -14,18 +15,20 @@ class Synonym:
         self.main()
 
     def req(self):
-        url = "https://www.thesaurus.com/browse/" + self.word
-        r = requests.get(url)
-        soup = BeautifulSoup(r.content, "lxml")
+        conn = http.client.HTTPSConnection("www.thesaurus.com")
+        url = "/browse/" + self.word
+        conn.request("GET", url)
+        html = conn.getresponse().read().decode("utf-8")
+        soup = Soup(html)
         js = ""
-        for i in soup.find_all("script"):
-            if "window.INITIAL_STATE" in str(i):
-                js = str(i)[31:-10].replace(":undefined", ':"undefined"')
+        for i in soup.find("script", mode="all"):
+            if "window.INITIAL_STATE" in i.text:
+                js = i.text[23:-1].replace(":undefined", ':"undefined"')
 
         try:
-            jsondata = json.loads(js)
-        except ValueError:
-            print("try again...")
+            jsondata = loads(js)
+        except Exception as e:
+            print(e)
             exit()
 
         try:
@@ -42,17 +45,17 @@ class Synonym:
                 }
             return dic
         except Exception:
-            didym = soup.find("span", class_="css-1umtscx e1rvdh510")
+            didym = soup.find("span", attrs={"class": "css-1umtscx e1rvdh510"})
             if didym is not None:
-                rprint("[cyan]" + didym.text)
+                rprint("[cyan]" + didym.text + " " + didym.find("a").text)
                 rprint(
                     "\n[cyan]More suggestions:\n"
                     + ", ".join(
                         [
                             i.text
                             for i in soup.find(
-                                "div", class_="css-5kov97 e1wla5061"
-                            ).find_all("a")
+                                "div", attrs={"class": "css-5kov97 e1wla5061"}
+                            ).find("a", mode="all")
                         ]
                     )
                     + "."
@@ -67,24 +70,25 @@ class Synonym:
             syn_lst = []
             for j in dic[i]["synonyms"]:
                 syn_lst.append(j[0])
-            print("⬤synonyms: " + ",".join(syn_lst))
-            if dic[i]["antonyms"] != []:
+            print("⬤synonyms: " + ", ".join(syn_lst))
+            if dic[i]["antonyms"] == []:
+                print()
+            else:
                 ant_lst = []
                 for j in dic[i]["antonyms"]:
                     ant_lst.append(j[0])
-                print("⬤antonyms: " + ",".join(ant_lst))
+                print("⬤antonyms: " + ", ".join(ant_lst))
                 print()
 
     def rich(self, dic):
-        # rprint("\t" + self.term + " | Thesaurus")
         for i in dic:
             colors = {
                 "100": ["[rgb(252,232,197)]", "[/rgb(252,232,197)]"],
                 "50": ["[rgb(220,221,187)]", "[/rgb(220,221,187)]"],
                 "10": ["[rgb(191,182,155)]", "[/rgb(191,182,155)]"],
-                "-100": ["", ""],
-                "-50": ["", ""],
-                "-10": ["", ""],
+                "-100": ["[grey74]", "[/grey74]"],
+                "-50": ["[grey58]", "[/grey58]"],
+                "-10": ["[grey42]", "[grey42]"],
             }
             # SQUARE, ROUNDED, SIMPLE_HEAD, MINIMAL_HEAVY_HEAD
             table = Table(box=box.SQUARE)
@@ -104,8 +108,8 @@ class Synonym:
             if dic[i]["antonyms"] != []:
                 table.add_row()
                 table.add_row(
-                    "🟤[cyan3]antonyms: "
-                    + "[grey50],[/grey50] ".join(
+                    "🟤[grey74]antonyms: [/grey74]"
+                    + "[grey74],[/grey74] ".join(
                         [
                             colors[tups[1]][0] + "".join(tups[0]) + colors[tups[1]][1]
                             for tups in dic[i]["antonyms"]
@@ -113,6 +117,10 @@ class Synonym:
                     )
                 )
             rprint(table)
+        Console().print(
+            f"[grey42][link=https://www.thesaurus.com/browse/{self.word}]thesaurus.com↗[/link]",
+            justify="right",
+        )
 
     def main(self):
         self.plain(self.req()) if self.arg_plain else self.rich(self.req())
