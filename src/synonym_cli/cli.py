e2c8917097ae833158ce540a6fd2dd92c3199c1d
Nick <ufohighway@protonmail.com>
2023-06-26 22:19:56 -0400

    Works with BeautifulSoup

--- src/synonym_cli/cli.py
+++ src/synonym_cli/cli.py
@@ -1,123 +1,77 @@
 from urllib.parse import quote
-from json import loads
-from gazpacho import Soup
-import http.client
+import requests
+from bs4 import BeautifulSoup
+import json
 from rich.table import Table
 from rich.console import Console
 from rich import box
 from rich import print as rprint
-
 
 class Synonym:
     def __init__(self, word):
         self.word = quote(word)
 
     def req(self):
-        conn = http.client.HTTPSConnection("www.thesaurus.com")
-        url = "/browse/" + self.word
-        conn.request("GET", url)
-        html = conn.getresponse().read().decode("utf-8")
-        soup = Soup(html)
-        js = ""
-        for i in soup.find("script", mode="all"):
-            if "window.INITIAL_STATE" in i.text:
-                js = i.text[23:-1].replace(":undefined", ':"undefined"')
+        
+        url = f"https://www.thesaurus.com/browse/{self.word}"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
 
-        try:
-            jsondata = loads(js)
-        except Exception as e:
-            print(e)
-            exit()
+        script = soup.find('script', {'id': 'preloaded-state'})
+        if script is not None:
+            script_text = script.string
 
-        try:
-            self.term = jsondata["searchData"]["searchTerm"]
-            tabs = jsondata["searchData"]["tunaApiData"]["posTabs"]
-            dic = {}
-            for i in tabs:
-                synlis = [(i["term"], i["similarity"]) for i in i["synonyms"]]
-                antlis = [(i["term"], i["similarity"]) for i in i["antonyms"]]
-                dic[i["definition"]] = {
-                    "pos": i["pos"],
-                    "synonyms": synlis,
-                    "antonyms": antlis,
-                }
-            return dic
-        except Exception:
-            didym = soup.find("span", attrs={"class": "css-1umtscx e1rvdh510"})
-            if didym is not None:
-                rprint("[cyan]" + didym.text + " " + didym.find("a").text)
-                rprint(
-                    "\n[cyan]More suggestions:\n"
-                    + ", ".join(
-                        [
-                            i.text
-                            for i in soup.find(
-                                "div", attrs={"class": "css-5kov97 e1wla5061"}
-                            ).find("a", mode="all")
-                        ]
-                    )
-                    + "."
-                )
-            else:
-                rprint("[cyan]not found")
-            exit()
+            start = script_text.find('{')
+            end = script_text.rfind('}') + 1
+            json_text = script_text[start:end]
 
-    def plain(self):
-        dic = self.req()
-        for i in dic:
-            print("❯" + i + " (" + dic[i]["pos"] + ")")
-            syn_lst = []
-            for j in dic[i]["synonyms"]:
-                syn_lst.append(j[0])
-            print("⬤synonyms: " + ", ".join(syn_lst))
-            if dic[i]["antonyms"] == []:
-                print()
-            else:
-                ant_lst = []
-                for j in dic[i]["antonyms"]:
-                    ant_lst.append(j[0])
-                print("⬤antonyms: " + ", ".join(ant_lst))
-                print()
+            data = json.loads(json_text)
+            
+            tuna_data = data['tuna']
+            results_data = tuna_data['resultsData']
+            if results_data is None:
+                    rprint(f"[cayn]{self.word} is not a word (according to thesaurus.com)")
+                    return []
+                
+            definition_data = results_data['definitionData']
+            definitions = definition_data['definitions']
+
+            colors = {
+                "100": "[rgb(252,232,197)]",
+                "50": "[rgb(220,221,187)]",
+                "10": "[rgb(191,182,155)]",
+                "-100": "[grey74]",
+                "-50": "[grey58]",
+                "-10": "[grey42]",
+            }
+
+            definitions_synonyms_antonyms = []
+            for definition in definitions:
+                def_text = definition.get('definition', 'N/A')
+                pos = definition.get('pos', 'N/A')
+                synonyms = [colors[syn['similarity']] + syn['term'] + "[/]" for syn in definition.get('synonyms', [])]
+                antonyms = [colors[ant['similarity']] + ant['term'] + "[/]" for ant in definition.get('antonyms', [])]
+                definitions_synonyms_antonyms.append((f"{pos}. {def_text}", synonyms, antonyms))
+            
+            return definitions_synonyms_antonyms
 
     def rich(self):
-        dic = self.req()
-        for i in dic:
-            colors = {
-                "100": ["[rgb(252,232,197)]", "[/rgb(252,232,197)]"],
-                "50": ["[rgb(220,221,187)]", "[/rgb(220,221,187)]"],
-                "10": ["[rgb(191,182,155)]", "[/rgb(191,182,155)]"],
-                "-100": ["[grey74]", "[/grey74]"],
-                "-50": ["[grey58]", "[/grey58]"],
-                "-10": ["[grey42]", "[grey42]"],
-            }
-            # SQUARE, ROUNDED, SIMPLE_HEAD, MINIMAL_HEAVY_HEAD
+        definitions_synonyms_antonyms = self.req()
+        console = Console()
+
+        for definition, synonyms, antonyms in definitions_synonyms_antonyms:
+            def_split = definition.split('.')
+            part_of_speech = def_split[0]
+            def_text = def_split[1].strip()
             table = Table(box=box.SQUARE)
-            # ! TODO: must be sorted by tuple[1]
-            table.add_column(
-                "[cyan]❯ " + i + "[/cyan]" + "[grey50]" + " (" + dic[i]["pos"] + ")"
-            )
-            table.add_row(
-                "🔵[cyan3]synonyms:[/cyan3] "
-                + "[grey50],[/grey50] ".join(
-                    [
-                        colors[tups[1]][0] + "".join(tups[0]) + colors[tups[1]][1]
-                        for tups in dic[i]["synonyms"]
-                    ]
-                )
-            )
-            if dic[i]["antonyms"] != []:
-                table.add_row()
-                table.add_row(
-                    "🟤[grey74]antonyms: [/grey74]"
-                    + "[grey74],[/grey74] ".join(
-                        [
-                            colors[tups[1]][0] + "".join(tups[0]) + colors[tups[1]][1]
-                            for tups in dic[i]["antonyms"]
-                        ]
-                    )
-                )
-            rprint(table)
-        Console().print(
+            table.add_column("[blue]❯ " + def_text + " [gray50](" + part_of_speech + ")")
+            table.add_row("🔵[cyan3]Synonyms:[/cyan3] " +
+                          "[grey50],[/grey50] ".join(synonyms))
+            if antonyms:
+                table.add_row("🟤[grey74]Antonyms:[/grey74] " +
+                              "[grey74],[/grey74] ".join(antonyms))
+            console.print(table)
+        console.print(
             f"[grey42][link=https://www.thesaurus.com/browse/{self.word}]thesaurus.com↗[/link]",
             justify="right",
         )
