2949702fae9b22591d2b9f6f44ea4222a1ab6988
Gökçe Merdun <agmmnn@gmail.com>
2023-07-07 13:08:49 +0300

    Update test_main.py

--- test/test_main.py
+++ test/test_main.py
@@ -3,6 +3,13 @@ from synonym_cli.cli import Synonym
 
 def test_req(capsys):
     with capsys.disabled():
-        assert type(Synonym("lighter").req()) == dict
-        assert Synonym("lighter").req()["large work boat"]["synonyms"][0][0] == "raft"
-        assert Synonym("lighter").req()["large work boat"]["synonyms"][0][1] == "100"
+        word = Synonym("lighter")
+        fetch = word.fetch_data()
+        data = word.parse_data(fetch)
+        results_data = data["tuna"]["resultsData"]
+        definition_data = results_data["definitionData"]
+        definitions = definition_data["definitions"]
+
+        assert type(definitions) == list
+        assert definitions[0]["synonyms"][0]["targetSlug"] == "raft"
+        assert definitions[0]["synonyms"][0]["similarity"] == "100"
